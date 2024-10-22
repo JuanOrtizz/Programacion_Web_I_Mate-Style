428de8d4a5f7546c83cc314d613e2ac5684ec99f
JuanOrtizz <juan_ignaortiz@outlook.com>
2024-10-22 00:31:33 -0300

    Correciones de asociacion de JS al HTML y funciones

--- Producto.js
+++ Producto.js
@@ -1,12 +1,24 @@
 class Producto{ // creo la clase producto, para manejar de forma mas modular el codigo.
     constructor(nombre, precio){
         this.nombre = nombre
-        this.precio = precio
+        this.precio = parseFloat(precio)
     }
+
     imprimirInformacion(){
         return "Nombre del producto: " + this.nombre + ", Precio del producto: $" +this.precio
     }
+
+    // actua como un set de nombre
+    cambiarNombre(nombre){
+        this.nombre = nombre
+    }
+
+    // actua como un set de precio
+    cambiarPrecio(precio){
+        this.precio = precio
+    }
 }
 
+export default Producto
 //Una consulta es si hay herencia en JS, y si es posible utilizarla, para cada tipo de producto, para asi diferenciar mas especificamente cada uno de estos
 // por ejemplo Mate, Bombilla, Termo, Accesorio, y que queda una de ellas hereden de Producto
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+import Producto from "./Producto.js" 
 // Dar un alert con la bienvenida a la pagina
 alert("Bienvenido")
 
 //creo los objetos dentro de un array para los productos.
-let productos = [
+const productos = [
     new Producto ('Mate Imperial Negro', 8500),
     new Producto ('Mate Imperial Rosa', 8500),
     new Producto ('Mate Seleccion Argentina', 5500),
@@ -10,7 +11,7 @@ let productos = [
 ]
 
 // creo el array para el carrito
-let carrito = [
+const carrito = [
     //esta vacio, pero se pueden ir agregando productos.
 ]
 
@@ -33,31 +34,56 @@ function filtroRangoPrecio (precioMinimo, precioMaximo){
     }
 }
 
+//funcion para mostrar todos los productos disponibles
+function mostrarProductos(){
+    let contador = 0
+    for (let i of productos) {
+        console.log("[" + contador + "] " + i.imprimirInformacion())
+        contador++
+    }
+}
+
 // funcion para agregar un nuevo producto usando el constructor y entradas del usuario. Esto va a ser solo para el administrador de la pagina en un futuro
 function crearNuevoProducto(){
     alert ("Estas por agregar un nuevo producto")
     let nombreTemp = prompt("Ingresa el nombre del producto: ")
-    let precioTemp = parseInt(prompt ("Ingresa el precio del producto: "))
+    let precioTemp = parseFloat(prompt ("Ingresa el precio del producto: "))
     productos.push(new Producto (nombreTemp, precioTemp))
     console.log("Producto agregado con exito!")
 }
 
 // funcion para agregar un producto al carrito por ahora con un indice, luego va a ser con el boton agregar al carrito
 function agregarProductoCarrito() {
-    alert("Estas por agregar un nuevo producto al carrito");
-    console.log("Estos son los productos: ");
-    let contador = 0;
-    for (let i of productos) {
-        console.log("[" + contador + "] " + i.imprimirInformacion());
-        contador++;
-    }
-    let indiceTemp = parseInt(prompt("Ingresa el indice del producto que deseas agregar: "));
+    alert("Estas por agregar un nuevo producto al carrito")
+    mostrarProductos()
+    let indiceTemp = parseInt(prompt("Ingresa el indice del producto que deseas agregar: "))
     if (indiceTemp >= 0 && indiceTemp < productos.length) {
-        carrito.push(productos[indiceTemp]);
-        console.log("Producto agregado al carrito: " + productos[indiceTemp].imprimirInformacion());
+        carrito.push(productos[indiceTemp])
+        console.log("Producto agregado al carrito: " + productos[indiceTemp].imprimirInformacion())
     } else {
-        console.log("Indice invalido, no se pudo agregar el producto al carrito.");
+        console.log("Indice invalido, no se pudo agregar el producto al carrito.")
     }
+}
+
+//Funcion para quitar un producto del carrito
+function quitarProductoCarrito(){
+    if(carrito.length !== 0){
+        alert("Estas por sacar un producto de tu carrito")
+        let contador = 0
+        for(let productoCarrito of carrito){
+            console.log("[" + contador + "]" + productoCarrito.imprimirInformacion())
+            contador++
+        }
+        let indiceTemp = parseInt(prompt("Ingresa el indice del producto que deseas quitar: "))
+        if (indiceTemp >= 0 && indiceTemp < carrito.length) {
+            carrito.splice(indiceTemp, 1)
+            console.log("Producto quitado del carrito")
+        } else {
+            console.log("Indice invalido, no se pudo quitar el producto del carrito.")
+        }
+    }else{
+        alert("El carrito esta vacio")
+    }   
 }
 
 // Funcion para mostrar todos los productos del carrito
@@ -72,6 +98,24 @@ function mostrarCarrito(){
     }
 }
 
+function cambiarNombreProducto(producto){
+    alert("Estas por actualizar un producto")
+    let nombreProductoTemp = prompt("Ingresa el nuevo nombre del producto: ")
+    producto.cambiarNombre(nombreProductoTemp)
+    console.log("Producto actualizado")
+}
+
+function cambiarPrecioProducto(producto){
+    alert("Estas por actualizar un producto")
+    let precioProductoTemp = parseFloat(prompt("Ingresa el nuevo precio del producto: "))
+    if (precioProductoTemp > 0){
+        producto.cambiarPrecio(precioProductoTemp)
+        console.log("Producto actualizado") 
+    }else{
+        console.log("El precio debe ser mayor a 0")
+    }
+}
+
 let booleanoWhile = true
 // Este do-while con su switch es temporal, pero es para mantener el flujo del programa para una correcta visualizacion desde la consola para esta entrega
 do{
@@ -80,7 +124,11 @@ do{
     console.log("2-Crear un producto nuevo")
     console.log("3-Agregar un producto al carrito")
     console.log("4-Ver tu carrito")
-    console.log("5-Salir")
+    console.log("5-Quitar producto del carrito")
+    console.log("6-Ver productos")
+    console.log("7-Modificar el nombre de un producto")
+    console.log("8-Modificar el precio de un producto")
+    console.log("9-Salir")
     let opcionMenu = parseInt(prompt("Selecciona una opcion e ingresa el numero de la misma: "))
     switch(opcionMenu){
         case 1:
@@ -99,7 +147,24 @@ do{
             mostrarCarrito()
             break
         case 5:
+            quitarProductoCarrito()
+            break
+        case 6:
+            mostrarProductos()
+            break
+        case 7:
+            mostrarProductos()
+            let indiceProdModNombre = parseInt(prompt("Coloca el indice del producto a modificar su nombre: "))
+            cambiarNombreProducto(productos[indiceProdModNombre])
+            break
+        case 8:
+            mostrarProductos()
+            let indiceProdModPrecio = parseInt(prompt("Coloca el indice del producto a modificar su precio: "))
+            cambiarPrecioProducto(productos[indiceProdModPrecio])
+            break
+        case 9:
             booleanoWhile = false
+            console.log("Saliendo...");
             break
         default:
             console.log("Opcion no valida, ingresa otra")
