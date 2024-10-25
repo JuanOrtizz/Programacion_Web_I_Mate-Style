98daf604b8257c8eef3d57e2accd54681aa8cb01
JuanOrtizz <juan_ignaortiz@outlook.com>
2024-10-25 15:05:26 -0300

    Cambios solicitados realizados para feature/tercer-entrega

--- script.js
+++ script.js
@@ -14,13 +14,34 @@ const carrito = [
 ]
 
 //funcion para filtrar en un rango de precios
-function filtroRangoPrecio (precioMinimo, precioMaximo){
+function filtroRangoPrecio (){
+    alert("Estas por filtrar productos por el precio")
+    let banderaPrecioMinFiltro = false
+    let banderaPrecioMaxFiltro = false
+    let precioMinimo
+    let precioMaximo
+    do{
+        precioMinimo = parseFloat(prompt("Ingresa el precio minimo: "))
+        if (precioMinimo < 0 || isNaN(precioMinimo)){
+            alert("El precio minimo debe ser un numero mayor o igual 0")
+        }else{
+            banderaPrecioMinFiltro = true
+        }
+    }while(!banderaPrecioMinFiltro)
+    do{
+        precioMaximo = parseFloat(prompt("Ingresa el precio maximo: "))
+        if (precioMaximo <= precioMinimo || isNaN(precioMaximo)){
+            alert("El precio maximo debe ser un numero mayor al precio minimo")
+        }else{
+            banderaPrecioMaxFiltro = true
+        }
+    }while(!banderaPrecioMaxFiltro)
     if (productos.length !== 0){ // Si el arreglo no esta vacio ejecuta el bloque de codigo
-        console.log("Estos son los productos en el rango de precio: ")
+        console.log("Estos son los productos en el rango de precio: " + "($" + precioMinimo + " - $" + precioMaximo + ")")
         let productosFiltrados = false //pongo una bandera para validar si muestra productos o no en ese rango
         for(let i = 0; i < productos.length; i++){
             if (productos[i].precio <= precioMaximo && productos[i].precio >= precioMinimo){
-                console.log(productos[i].imprimirInformacion())
+                console.table(productos[i])
                 productosFiltrados = true
             }
         }
@@ -28,121 +49,161 @@ function filtroRangoPrecio (precioMinimo, precioMaximo){
             console.log("No hay productos en el rango especificado")
         }
     }else{ // si el arreglo esta vacio imprime este console.log
-        console.log("No hay objetos en esta seccion.")
+        console.log("No hay productos disponibles")
     }
 }
 
 //funcion para mostrar todos los productos disponibles
 function mostrarProductos(){
-    let contador = 0
-    for (let i of productos) {
-        console.log("[" + contador + "] " + i.imprimirInformacion())
-        contador++
-    }
+    console.log("Estos son los productos: ")
+    console.table(productos)
 }
 
 // funcion para agregar un nuevo producto usando el constructor y entradas del usuario. Esto va a ser solo para el administrador de la pagina en un futuro
 function crearNuevoProducto(){
     alert ("Estas por agregar un nuevo producto")
-    let nombreTemp = prompt("Ingresa el nombre del producto: ")
-    let precioTemp = parseFloat(prompt ("Ingresa el precio del producto: "))
+    let banderaNombre = false
+    let banderaPrecio = false
+    let nombreTemp
+    let precioTemp
+    do{
+        let nombrePrompt = prompt("Ingresa el nombre del producto: ")
+        if (nombrePrompt === ""){ // si nombre esta vacio ejecuta el alert
+            alert("El nombre no puede estar vacio")
+        }else{
+            banderaNombre = true
+            nombreTemp = nombrePrompt
+        }
+    }while(!banderaNombre)
+    do{
+        let precioPrompt = parseFloat(prompt ("Ingresa el precio del producto: "))
+        if (precioPrompt <= 0 || isNaN(precioPrompt) ){ // si precio es menor o igual a 0 o si precio no es un numero ejecuta el alert
+            alert("El precio debe ser un numero mayor a 0")
+        }else{
+            banderaPrecio = true
+            precioTemp = precioPrompt
+        }
+    }while(!banderaPrecio)
     productos.push(new Producto (nombreTemp, precioTemp))
     console.log("Producto agregado con exito!")
+    console.table(productos)
 }
 
+//funcion para calcular el total del carrito
 function calcularTotalCarrito(){
     let totalCarrito = 0
-    for(let producto of carrito){
-        totalCarrito += producto.precio
+    if(carrito.length !== 0){
+        for(let producto of carrito){
+            totalCarrito += producto.precio
+        }
     }
     console.log("Total: " + totalCarrito.toFixed(2)) // para que tenga 2 decimales
 }
 
-// funcion para agregar un producto al carrito por ahora con un indice, luego va a ser con el boton agregar al carrito
+//funcion para agregar un producto al carrito por ahora con un indice, luego va a ser con el boton agregar al carrito
 function agregarProductoCarrito() {
     alert("Estas por agregar un nuevo producto al carrito")
     mostrarProductos()
     let indiceTemp = parseInt(prompt("Ingresa el indice del producto que deseas agregar: "))
     if (indiceTemp >= 0 && indiceTemp < productos.length) {
         carrito.push(productos[indiceTemp])
-        console.log("Producto agregado al carrito: " + productos[indiceTemp].imprimirInformacion())
+        console.log("Producto agregado al carrito: ")
+        console.table(carrito)
     } else {
         console.log("Indice invalido, no se pudo agregar el producto al carrito.")
     }
 }
 
-//Funcion para quitar un producto del carrito
+//funcion para quitar un producto del carrito
 function quitarProductoCarrito(){
     if(carrito.length !== 0){
         alert("Estas por sacar un producto de tu carrito")
-        let contador = 0
-        for(let productoCarrito of carrito){
-            console.log("[" + contador + "]" + productoCarrito.imprimirInformacion())
-            contador++
-        }
+        console.table(carrito)
         let indiceTemp = parseInt(prompt("Ingresa el indice del producto que deseas quitar: "))
         if (indiceTemp >= 0 && indiceTemp < carrito.length) {
             carrito.splice(indiceTemp, 1)
             console.log("Producto quitado del carrito")
         } else {
-            console.log("Indice invalido, no se pudo quitar el producto del carrito.")
+            console.log("Indice invalido, no se pudo quitar el producto del carrito")
         }
     }else{
         alert("El carrito esta vacio")
     }   
 }
 
-// Funcion para mostrar todos los productos del carrito
+//funcion para mostrar todos los productos del carrito
 function mostrarCarrito(){
     console.log("---Tu Carrito---")
     if(carrito.length !== 0){
-        for(let productoCarrito of carrito){
-            console.log(productoCarrito.imprimirInformacion())
-        }
-        calcularTotalCarrito()
+        console.table(carrito)
     }else{
         console.log("No hay productos en tu carrito")
     }
+    calcularTotalCarrito()
 }
 
+//funcion para cambiar el nombre de un producto
 function cambiarNombreProducto(producto){
-    alert("Estas por actualizar un producto")
-    let nombreProductoTemp = prompt("Ingresa el nuevo nombre del producto: ")
-    producto.cambiarNombre(nombreProductoTemp)
+    let banderaNombreInt = false
+    do{
+        let nombreProductoTemp = prompt("Ingresa el nuevo nombre del producto: ")
+        if (nombreProductoTemp === ""){ // si nombre esta vacio ejecuta el alert
+            alert("El nombre no puede estar vacio")
+        }else{
+            banderaNombreInt = true
+            producto.cambiarNombre(nombreProductoTemp)
+        }
+    }while(!banderaNombreInt)
     console.log("Producto actualizado")
 }
 
+//funcion para cambiar el precio de un producto
 function cambiarPrecioProducto(producto){
-    alert("Estas por actualizar un producto")
-    let precioProductoTemp = parseFloat(prompt("Ingresa el nuevo precio del producto: "))
-    if (precioProductoTemp > 0){
-        producto.cambiarPrecio(precioProductoTemp)
-        console.log("Producto actualizado") 
-    }else{
-        console.log("El precio debe ser mayor a 0")
-    }
+    let banderaPrecioInt = false
+    do{
+        let precioProductoTemp = parseFloat(prompt("Ingresa el nuevo precio del producto: "))
+        if (precioProductoTemp <= 0 || isNaN(precioProductoTemp)){  // si precio es menor o igual a 0 o si precio no es un numero ejecuta el alert
+            alert("El precio debe ser un numero mayor a 0")
+        }else{
+            banderaPrecioInt = true
+            producto.cambiarPrecio(precioProductoTemp)
+        }
+    }while(!banderaPrecioInt)
+    console.log("Producto actualizado")
 }
 
+//funcion para vaciar el carrito
+function vaciarCarrito(){
+    //cual es mas conveniente usar, asignarle un nuevo arreglo vacio, cambiar la longitud a 0 o hacer un splice(0)?
+}
+
+//funcion para eliminar un producto
+function eliminarProducto(){
+    if(productos.length !== 0){
+        alert("Estas por eliminar un producto")
+        console.table(productos)
+        let indiceTemp = parseInt(prompt("Ingresa el indice del producto que deseas eliminar: "))
+        if (indiceTemp >= 0 && indiceTemp < productos.length) {
+            productos.splice(indiceTemp, 1)
+            console.log("Producto eliminado con exito")
+            console.table(productos)
+        } else {
+            console.log("Indice invalido, no se pudo eliminar el producto")
+        }
+    }else{
+        alert("No hay productos")
+    }   
+}
+
+// constante con las opciones del menu para que se muestre en el prompt del do-while
+const opciones = "------------------------MENU DE OPCIONES------------------------\n 1-Filtrar producto por precio\n 2-Crear un producto nuevo\n 3-Agregar un producto al carrito\n 4-Ver tu carrito\n 5-Quitar producto del carrito\n 6-Eliminar un producto\n 7-Ver productos\n 8-Modificar el nombre de un producto\n 9-Modificar el precio de un producto\n 10-Salir\n"
 let booleanoWhile = true
 // Este do-while con su switch es temporal, pero es para mantener el flujo del programa para una correcta visualizacion desde la consola para esta entrega
 do{
-    console.log("---Menu de Opciones---")
-    console.log("1-Filtrar producto por precio")
-    console.log("2-Crear un producto nuevo")
-    console.log("3-Agregar un producto al carrito")
-    console.log("4-Ver tu carrito")
-    console.log("5-Quitar producto del carrito")
-    console.log("6-Ver productos")
-    console.log("7-Modificar el nombre de un producto")
-    console.log("8-Modificar el precio de un producto")
-    console.log("9-Salir")
-    let opcionMenu = parseInt(prompt("Selecciona una opcion e ingresa el numero de la misma: "))
+    let opcionMenu = parseInt(prompt(opciones + "Selecciona una opcion e ingresa el numero de la misma: "))
     switch(opcionMenu){
         case 1:
-            alert("Estas por filtrar productos por el precio")
-            let precioMinimo = parseInt(prompt("Ingresa el precio minimo: "))
-            let precioMaximo = parseInt(prompt ("Ingresa el precio maximo: "))
-            filtroRangoPrecio(precioMinimo, precioMaximo)
+            filtroRangoPrecio()
             break
         case 2:
             crearNuevoProducto()
@@ -157,19 +218,34 @@ do{
             quitarProductoCarrito()
             break
         case 6:
-            mostrarProductos()
+            eliminarProducto()
             break
         case 7:
             mostrarProductos()
-            let indiceProdModNombre = parseInt(prompt("Coloca el indice del producto a modificar su nombre: "))
-            cambiarNombreProducto(productos[indiceProdModNombre])
             break
         case 8:
             mostrarProductos()
-            let indiceProdModPrecio = parseInt(prompt("Coloca el indice del producto a modificar su precio: "))
-            cambiarPrecioProducto(productos[indiceProdModPrecio])
+            alert("Estas por actualizar el nombre de un producto")
+            let indiceProdModNombre = parseInt(prompt("Coloca el indice del producto a modificar su nombre: "))
+            console.log("Producto seleccionado: " + indiceProdModNombre);
+            if(indiceProdModNombre < 0 || indiceProdModNombre > productos.length-1 || isNaN(indiceProdModNombre)){
+                alert("El indice que proporcionaste no es valido")
+            }else{
+                cambiarNombreProducto(productos[indiceProdModNombre])
+            }
             break
         case 9:
+            mostrarProductos()
+            alert("Estas por actualizar el precio de un producto")
+            let indiceProdModPrecio = parseInt(prompt("Coloca el indice del producto a modificar su precio: "))
+            console.log("Producto seleccionado: " + indiceProdModPrecio);
+            if(indiceProdModPrecio < 0 || indiceProdModPrecio > productos.length-1 || isNaN(indiceProdModPrecio)){
+                alert("El indice que proporcionaste no es valido")
+            }else{
+                cambiarPrecioProducto(productos[indiceProdModPrecio])
+            }
+            break
+        case 10:
             booleanoWhile = false
             console.log("Saliendo...");
             break
